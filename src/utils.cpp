5f2df9cded13b2b431e85f197cd6b623555d2b44
Johan Jönsson <johanjoensson@gmail.se>
2020-06-05 19:51:13 +0200

    slight improvements

--- src/utils.cpp
+++ src/utils.cpp
@@ -3,26 +3,6 @@
 #include "ewald_int.h"
 #include <iostream>
 
-std::ostream& operator<<(std::ostream& os, const lm& l)
-{
-    return os << "(" << l.l << ", " << l.m <<")";
-}
-
-bool operator==(const lm &a, const lm &b)
-{
-    return (a.l == b.l) && (a.m == b.m);
-}
-
-bool operator!=(const lm &a, const lm &b)
-{
-    return !(a == b);
-}
-
-double lerp(double x, double x0, double x1, double v0, double v1)
-{
-    return 1./(x1 - x0) * ((x1 - x)*v0 + (x - x0)*v1);
-}
-
 double calc_eta(const double vol)
 {
 	return GSL::exp(GSL::log(6.5) + 2./3*GSL::log(4*M_PI/3) -
--- src/utils.h
+++ src/utils.h
@@ -10,19 +10,49 @@ enum spin{
 };
 
 struct lm {
+    lm(int l_n, int m_n):n(0), l(l_n), m(m_n){}
+    lm():n(0), l(0), m(0){}
+    int n;
     int l;
 	int m;
+
+    bool operator==(const lm &a) const
+    {
+        return (this->n == a.n) && (this->l == a.l) && (this->m == a.m);
+    }
+
+    bool operator!=(const lm &a) const
+    {
+        return !(*this == a);
+    }
 };
 
-std::ostream& operator<<(std::ostream& os, const lm& l);
+inline std::ostream& operator<<(std::ostream& os, const lm& l)
+{
+    return os << "(" << l.n << ", " << l.l << ", " << l.m <<")";
+}
 
-bool operator==(const lm &a, const lm &b);
-bool operator!=(const lm &a, const lm &b);
+struct lm_hash{
+    size_t operator()(const lm& l)
+    {
+        size_t res = 0;
+        res ^= std::hash<int>()(l.n) + 0x9e3779b9 + (res<< 6) + (res>> 2);
+        res ^= std::hash<int>()(l.l) + 0x9e3779b9 + (res<< 6) + (res>> 2);
+        res ^= std::hash<int>()(l.m) + 0x9e3779b9 + (res<< 6) + (res>> 2);
+        return res;
+    }
+};
 
-double lerp(double x, double x0, double x1, double v0, double v1);
+template<class X = double, class Y = double>
+double lerp(X x, X x0, X x1, Y y0, Y y1)
+{
+    return 1/(x1 - x0) * ((x1 - x)*y0 + (x - x0)*y1);
+}
 
 double calc_eta();
 
 double calc_Rmax(const double vol, const double kappa, const lm &l, const double tol);
 double calc_Kmax(const double vol, const double kappa, const lm &l, const double tol);
+
+
 #endif // UTILS_H
