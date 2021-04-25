cf9f069e9fd16de1f4e02b7fecc8bec70711114b
Johan Jönsson <johanjoensson@gmail.com>
2021-04-25 23:43:48 +0200

    Updated atomic code to new dirac

--- bin/atomic.cpp
+++ bin/atomic.cpp
@@ -6,12 +6,16 @@
 
 int main()
 {
+    const double c = 2*137.035999174;
+    const double alpha = 2./c;
+
     unsigned int z = 1;
+
     auto V = [&] (const double r) {
         return -2./r;
     };
 
-    Exponential_mesh<1, double> m{0, 40, 0.001, 10001};
+    Exponential_mesh<1, double> m{0, 250, 0.001, 10001};
     std::vector<double> pot;
     std::transform(m.begin(), m.end(), std::back_inserter(pot),
     [=](const Exponential_mesh<1, double>::mesh_point p)
@@ -23,14 +27,18 @@ int main()
 
     std::cout << "epsilon = " << std::numeric_limits<double>::epsilon() << "\n";
 
-    int kappa = -1;
-    const double c = 2*137.035999174;
+
+    int n = 5, l = 2, s = -1;
+    int kappa = -l - 1;
+    if(s < 0){
+        kappa = l;
+    }
     auto beta = sqrt(GSL::pow_int(kappa, 2) - GSL::pow_int(2/c, 2));
     auto rb = GSL::exp(beta*GSL::log(m.r(1))).val;
     double lambda = sqrt(-0.5 - GSL::pow_int(-0.5/c, 2));
     std::array<std::vector<double>, 2> left_init {
-                                        std::vector<double>{0, rb/sqrt(m.dr(1))},
-                                        std::vector<double>{0, rb*c*(beta + kappa)/sqrt(m.dr(1))}
+                                        std::vector<double>{0, rb},
+                                        std::vector<double>{0, rb*c*(beta + kappa)}
                                     };
     std::array<std::vector<double>, 2> right_init {
                                             std::vector<double>{2*std::numeric_limits<double>::epsilon()},
@@ -39,7 +47,13 @@ int main()
 
     Radial_Dirac_Equation dirac{m, pot, kappa, left_init, right_init, std::numeric_limits<double>::epsilon()};
 
-    dirac.solve(2, -1.0, -1.1, 0);
+    dirac.solve(n - l - 1, -1./GSL::pow_int(n, 2), -1.1, 0);
+
+    double exact_energy = -1./GSL::pow_int(n, 2)*(
+         1 + GSL::pow_int(z*alpha, 2)/n*(1./std::abs(kappa) - 3./(4*n)));
+    std::cout << "Energy = " << dirac.energy() << "(Ry), error ~ " <<
+        std::abs(dirac.energy() - exact_energy)*1e6 << "(\u03BCRy) \n";
+    std::cout << "N = " << dirac.norm() << "\n";
     dirac.normalize();
     std::ofstream os;
     os.open("dirac_test.dat", std::ios::out);
