faf6c3ec3a1ea8fad4777f43b812bd3e0b2a90e2
Johan Jönsson <johanjoensson@gmail.com>
2021-04-30 23:48:00 +0200

    Simple test of Poisson solver added

--- bin/atomic.cpp
+++ bin/atomic.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
+#include <poisson.h>
 
 int main()
 {
@@ -15,7 +16,7 @@ int main()
         return -2./r;
     };
 
-    Exponential_mesh<1, double> m{0, 250, 0.001, 10001};
+    Exponential_mesh<1, double> m{0, 150, 0.01, 1001};
     std::vector<double> pot;
     std::transform(m.begin(), m.end(), std::back_inserter(pot),
     [=](const Exponential_mesh<1, double>::mesh_point p)
@@ -28,7 +29,7 @@ int main()
     std::cout << "epsilon = " << std::numeric_limits<double>::epsilon() << "\n";
 
 
-    int n = 5, l = 2, s = -1;
+    int n = 5, l = 0, s = 1;
     int kappa = -l - 1;
     if(s < 0){
         kappa = l;
@@ -41,11 +42,11 @@ int main()
                                         std::vector<double>{0, rb*c*(beta + kappa)}
                                     };
     std::array<std::vector<double>, 2> right_init {
-                                            std::vector<double>{2*std::numeric_limits<double>::epsilon()},
-                                            std::vector<double>{-2*std::numeric_limits<double>::epsilon()}
+                                            std::vector<double>{1e-16},
+                                            std::vector<double>{-1e-18}
                                         };
 
-    Radial_Dirac_Equation dirac{m, pot, kappa, left_init, right_init, std::numeric_limits<double>::epsilon()};
+    Radial_Dirac_Equation dirac{m, pot, kappa, left_init, right_init, 1e-8};
 
     dirac.solve(n - l - 1, -1./GSL::pow_int(n, 2), -1.1, 0);
 
@@ -55,11 +56,21 @@ int main()
         std::abs(dirac.energy() - exact_energy)*1e6 << "(\u03BCRy) \n";
     std::cout << "N = " << dirac.norm() << "\n";
     dirac.normalize();
+
+    std::cout << "Calculate density!\n";
+    std::vector<double> rho(m.size());
+    for(size_t i = 0; i < m.size(); i++){
+        rho[i] = dirac.g(i)*dirac.g(i) + dirac.f(i)*dirac.f(i);
+    }
+    std::cout << "Setting up Poisson equation" << std::endl;
+    Poisson_equation pos(m, rho, 0);
+    pos.solve();
     std::ofstream os;
     os.open("dirac_test.dat", std::ios::out);
     for(size_t i = 0; i < m.size(); i++){
         auto dr = m.dr(i);
-        os << m.r(i) << "  " << pot[i] << "  " << dirac.g(i)*sqrt(dr) << "  " << dirac.f(i)*sqrt(dr) << "\n";
+        os << m.r(i) << "  " << pot[i] << "  " << dirac.g(i) << "  " <<
+              dirac.f(i) << "  " << rho[i] << "  " << pos.val(i) << "\n";
     }
     os.close();
     return 0;
